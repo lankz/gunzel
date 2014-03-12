f7ce9b6d978d8631cfbecbc92a04d5319cabb7bc
Ben Caldwell <bencaldwell@einti.com>
2014-03-13 01:03:04 +1100

    Add a controller for statics and a simple root/home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Zangetsu::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'statics#home'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
