1060b8cabb3419bec17ae43a3ebcd85d358823b5
Ben Caldwell <bencaldwell@einti.com>
2014-03-12 22:05:59 +1100

    Tweak unicorn configuration file
    See https://devcenter.heroku.com/articles/getting-started-with-rails4#webserver

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
-worker_processes 3
-timeout 30
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
 preload_app true
 
 before_fork do |server, worker|
@@ -14,7 +14,7 @@ end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
   defined?(ActiveRecord::Base) and
