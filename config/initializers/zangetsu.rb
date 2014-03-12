fe17a1652a3e9cad62a31be5ba5cf564819eaa67
Ben Caldwell <bencaldwell@einti.com>
2014-03-12 22:42:16 +1100

    Don't generate quite so much cruft when scaffolding

--- config/initializers/zangetsu.rb
+++ config/initializers/zangetsu.rb
@@ -1,3 +1,15 @@
 Zangetsu::Application.config.generators do |g|
   g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+
+  # don't generate quite so much cruft when scaffolding
+  g.javascripts false
+  g.stylesheets false
+  g.helper false
+
+  # don't go overboard with generated specs either
+  g.test_framework :rspec,
+    :helper_specs => false,
+    :request_specs => false,
+    :routing_specs => false,
+    :view_specs => false
 end
