2a286ddb9f0355b93aff510e4be4eff61c113d75
Ben Caldwell <bencaldwell@einti.com>
2014-03-17 16:03:38 +1100

    Tweak application-wide config. Set include_all_helpers to false

--- config/initializers/zangetsu.rb
+++ config/initializers/zangetsu.rb
@@ -1,17 +1,22 @@
-Zangetsu::Application.config.generators do |g|
-  g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+Rails.application.instance_eval do
+  # because including all helpers, all the time is a bad idea
+  config.action_controller.include_all_helpers = false
 
-  # don't generate quite so much cruft when scaffolding
-  g.javascripts false
-  g.stylesheets false
-  g.helper false
+  config.generators do |g|
+    g.fixture_replacement :factory_girl, :dir => 'spec/factories'
 
-  # don't go overboard with generated specs either
-  g.test_framework :rspec,
-    :helper_specs => false,
-    :request_specs => false,
-    :routing_specs => false,
-    :view_specs => false
+    # don't generate quite so much cruft when scaffolding
+    g.javascripts false
+    g.stylesheets false
+    g.helper false
 
-  g.scaffold_controller = 'scaffold_controller'
+    # don't go overboard with generated specs either
+    g.test_framework :rspec,
+      :helper_specs => false,
+      :request_specs => false,
+      :routing_specs => false,
+      :view_specs => false
+
+    g.scaffold_controller = 'scaffold_controller'
+  end
 end
