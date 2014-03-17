80b1b135976814178015dc576eeea94330c0909e
Ben Caldwell <bencaldwell@einti.com>
2014-03-17 16:07:07 +1100

    Make ActionController not include all helpers by default

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Zangetsu
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # because including all helpers, all the time is a bad idea
+    config.action_controller.include_all_helpers = false
   end
 end
