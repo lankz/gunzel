f1957a942a3b24a72b091ee6214a27fc0f3ee225
Ben Caldwell <bencaldwell@einti.com>
2014-05-19 12:06:08 +1000

    Added support for user roles, management using rolify

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519012208) do
+ActiveRecord::Schema.define(version: 20140519020326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,17 @@ ActiveRecord::Schema.define(version: 20140519012208) do
   add_index "ingstor_1_items", ["crawler_id"], name: "index_ingstor_1_items_on_crawler_id", using: :btree
   add_index "ingstor_1_items", ["response_id"], name: "index_ingstor_1_items_on_response_id", using: :btree
 
+  create_table "roles", force: true do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
+
   create_table "scrapers", force: true do |t|
     t.string   "name"
     t.text     "definition"
@@ -155,6 +166,13 @@ ActiveRecord::Schema.define(version: 20140519012208) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users_roles", id: false, force: true do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
   create_table "versions", force: true do |t|
     t.string   "item_type",  null: false
