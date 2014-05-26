29d309f651286680d51c9af64a3f0759f0db8d01
Ben Caldwell <bencaldwell@einti.com>
2014-05-26 14:17:05 +1000

    Users now have a username. Allow login using username or email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,18 @@ class User < ActiveRecord::Base
   # associations ...............................................................
   # scopes .....................................................................
   # validations ................................................................
+
+  validates :username,
+            :presence => true,
+            :length => { :maximum => 255 },
+            :uniqueness => {
+              :case_sensitive => false
+            }
+
   # callbacks ..................................................................
   # additional config ..........................................................
+
+  attr_accessor :login
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -16,6 +26,17 @@ class User < ActiveRecord::Base
   rolify
 
   # class methods ..............................................................
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
   # instance methods ...........................................................
   # protected instance methods .................................................
   # private instance methods ...................................................
--- db/migrate/20140519012208_devise_create_users.rb
+++ db/migrate/20140519012208_devise_create_users.rb
@@ -2,6 +2,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
+      t.string :username,           null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -34,6 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    add_index :users, :username,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
