168307d87fdac02e64928ae21e6492d6cb8492f5
Ben Caldwell <bencaldwell@einti.com>
2014-05-28 12:46:35 +1000

    Don't attempt authorization check for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  check_authorization
+  check_authorization :unless => :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
