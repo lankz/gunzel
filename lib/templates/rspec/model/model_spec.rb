7376b19c9cf27d344f3007e4761c4f614b49c216
Ben Caldwell <bencaldwell@einti.com>
2014-07-10 16:54:52 +1000

    Customised template for model specs

---
+++ lib/templates/rspec/model/model_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+<% module_namespacing do -%>
+RSpec.describe <%= class_name %>, :type => :model do
+  subject { described_class }
+
+  context 'initialized' do
+    subject { build(:<%= singular_table_name %>) }
+  end
+
+  context 'persisted' do
+    subject { create(:<%= singular_table_name %>) }
+  end
+
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+<% end -%>
