dbb728f5e6acf92fdd25e4ebfb4da4a64ae37098
Ben Caldwell <bencaldwell@einti.com>
2014-07-10 16:54:23 +1000

    Include spec helpers for FactoryGirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,4 +40,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # So we don't have to use the FactoryGirl namespace everywhere.
+  config.include FactoryGirl::Syntax::Methods
 end
