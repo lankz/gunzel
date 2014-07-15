0fd7f583a4906b1b8df5f527682d236f2cf8d709
Ben Caldwell <bencaldwell@einti.com>
2014-07-15 10:16:09 +1000

    Configure RSpec to use random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # end of the spec run, to help surface which specs are running
   # particularly slow.
   config.profile_examples = 10
-
+=end
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
@@ -51,7 +51,7 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-
+=begin
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
