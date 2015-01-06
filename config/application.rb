5e21a68c206480d6edd538fe8a66eda59c185f5b
Ben Caldwell <bencaldwell@einti.com>
2015-01-06 15:54:40 +1100

    Ran the rails:update rake task

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Zangetsu
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     # because including all helpers, all the time is a bad idea
     config.action_controller.include_all_helpers = false
   end
