4feb410fc77ae56ddcfbaba26533275e942bcf08
Ben Caldwell <ben@einti.com>
2015-06-10 15:54:58 +1000

    Prevent scaffold from adding certain "internal" attributes to views

--- lib/generators/zangetsu/scaffold/scaffold_generator.rb
+++ lib/generators/zangetsu/scaffold/scaffold_generator.rb
@@ -7,8 +7,25 @@ module Zangetsu
         File.expand_path("../templates", __FILE__)
       end
 
+      def visible_attributes
+        attributes - hidden_attributes
+      end
+
+      def hidden_attributes
+        attributes.index_by(&:name) \
+                  .slice(*hidden_attribute_names) \
+                  .values
+      end
+
+      protected
+
       def available_views
         %w(index edit show new _table)
+      end
+
+      def hidden_attribute_names
+        %w(children_count deleted_at depth lft lock_version parent_id position
+           rgt slug)
       end
     end
   end
