36f7ce541f10ac82c24f0f0e9e15b7b680a873fd
Ben Caldwell <bencaldwell@einti.com>
2015-07-10 22:22:08 +1000

    Simplify the scaffold controller template a bit

--- lib/generators/rails/zangetsu_controller/templates/controller.rb
+++ lib/generators/rails/zangetsu_controller/templates/controller.rb
@@ -26,58 +26,46 @@ class <%= controller_class_name %>Controller < ApplicationController
   def index
     authorize!(:index, <%= class_name %>)
 
-    respond_with(@<%= plural_table_name %> ||= apply_scopes(
-      search_<%= plural_table_name %>.result \
-             <%= ' ' * plural_table_name.length %>.accessible_by(current_ability)))
+    @search ||= <%= class_name %>.accessible_by(current_ability).ransack(params[:q])
+    respond_with(@<%= plural_table_name %> ||= apply_scopes(@search.result))
   end
 
   def show
-    authorize!(:show, find_<%= singular_table_name %>)
-    respond_with(@<%= singular_table_name %>)
+    respond_with(authorize!(:show, @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>))
   end
 
   def new
-    authorize!(:new, @<%= singular_table_name %> ||= <%= orm_class.build(class_name) %>)
-    respond_with(@<%= singular_table_name %>)
+    respond_with(authorize!(:new, @<%= singular_table_name %> ||= <%= orm_class.build(class_name) %>))
   end
 
   def edit
-    authorize!(:edit, find_<%= singular_table_name %>)
-    respond_with(@<%= singular_table_name %>)
+    respond_with(authorize!(:edit, @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>))
   end
 
   def create
     authorize!(:create, @<%= singular_table_name %> ||= <%= orm_class.build(class_name, "#{singular_table_name}_params") %>)
 
     @<%= singular_table_name %>.save
-    respond_with(@<%= singular_table_name %>, :location => @<%= singular_table_name %>)
+    respond_with(@<%= singular_table_name %>)
   end
 
   def update
-    authorize!(:update, find_<%= singular_table_name %>)
+    authorize!(:update, @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>)
 
     @<%= orm_instance.update("#{singular_table_name}_params") %>
-    respond_with(@<%= singular_table_name %>, :location => @<%= singular_table_name %>)
+    respond_with(@<%= singular_table_name %>)
   end
 
   def destroy
-    authorize!(:destroy, find_<%= singular_table_name %>)
+    authorize!(:destroy, @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>)
 
     @<%= orm_instance.destroy %>
-    respond_with(@<%= singular_table_name %>, :location => <%= index_helper %>_url)
+    respond_with(@<%= singular_table_name %>)
   end
 
   # protected instance methods .................................................
 
   protected
-
-  def find_<%= singular_table_name %>
-    @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>
-  end
-
-  def search_<%= plural_table_name %>
-    @search ||= <%= class_name %>.ransack(params[:q])
-  end
 
   def <%= "#{singular_table_name}_params" %>
     <%- if attributes_names.empty? -%>
